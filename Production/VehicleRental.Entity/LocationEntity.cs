19aa34cb531bd5cfaa5f02ae7ffd750fff41fe9c
Arjun Singh <arjun.s.singh@outlook.com>
2025-08-14 20:48:42 +0530

    Included timestamp in location history api

--- Production/VehicleRental.Entity/LocationEntity.cs
+++ Production/VehicleRental.Entity/LocationEntity.cs
@@ -10,5 +10,6 @@ namespace VehicleRental.Entity
     {
         public double? Latitude { get; set; }
         public double? Longitude { get; set; }
+        public DateTime Timestamp { get; set; }
     }
 }
